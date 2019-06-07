40ad03668ac4a38fe5f28c641ad77adeeb74ee0e
Maurits van Rees <maurits@vanrees.org>
2019-06-07 14:15:59 +0200

    Back to development: 1.0.0a2
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.contentgroups",
-    version="1.0.0a1",
+    version="1.0.0a2.dev0",
     description="Plone PAS plugin for content as groups",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
